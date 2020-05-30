5cc6d51a74a26c2c6a7265358c57ea5c36680b88
mrkinix <hodhodbenchiboub@gmail.com>
2020-05-30 22:32:37 +0100

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,127 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+          <img v-if="visib" class="orange" :src="require('@/assets/portal.png')">
+      <img v-if="visib" :src="require('@/assets/portal.png')">
+    <div class="o"
+    :style="{transform: `translate(${x}vw, ${y}vw)`}"
+    ></div>
+    <LottieAnimation
+    :path="'./' + state + '.json'"
+    :loop="auto"
+    :autoPlay="true"
+    :speed="2"
+    :width="200"
+    :height="200"
+    id="anim"
+    :style="{right: pos + 'vw', top: pos2 + 'vh'}"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LottieAnimation from '../node_modules/lottie-vuejs/src/LottieAnimation.vue' // import lottie-vuejs
+import delay from 'delay'
 
 export default {
-  name: 'App',
+  data() {
+    return {
+      pos: 0,
+      state: 'moving',
+      auto: true,
+      pos2: 50,
+      x: 0,
+      y: 0,
+      visib: false
+    }
+  },
   components: {
-    HelloWorld
-  }
+    LottieAnimation
+  },
+  async mounted() {
+    for (let i = 0; i <100; ++i) {
+      await delay(17)
+      this.pos += .3
+    }
+    this.state = 'eyeroll'
+    this.auto = false
+    await delay(1000)
+    this.state = 'kick'
+    await delay(200)
+    for (let i = 0; i <100; ++i) {
+      await delay(7)
+      this.x += .3 + Math.random() * .2
+      this.y -= .3 + Math.random() * .2
+    }
+    await delay(1000)
+    for (let i = 0; i <30; ++i) {
+      await delay(7)
+      this.pos -= .1
+    }
+    for (let i = 0; i <30; ++i) {
+      await delay(7)
+      this.pos2 -= .1
+    }
+    this.state = 'jump'
+    await delay(2000)
+    this.visib = true
+    this.state = 'data'
+    this.auto = true
+    for (let i = 0; i <100; ++i) {
+      await delay(7)
+      this.pos2 += .55
+    }
+    for (let i = 0; i < 69; ++i) {
+      await this.loop()
+    }
+  },
+  methods: {
+    loop: async function() {
+      this.pos2 = -10;
+      for (let i = 0; i <200; ++i) {
+        await delay(7)
+        this.pos2 += .55
+      }
+    }
+  },
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background: black;
+  height: 100%;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  left:0;
 }
+#anim {
+  position: fixed;
+  top: 50vh;
+}
+
+.o {
+  height: 300px;
+  width: 200px;
+  background: rgb(65, 59, 59);
+  position: fixed;
+  top: calc(50% - 130px);
+  right: calc(30% - 150px);
+}
+
+img {
+  position: absolute;
+  height: 200px;
+  right: 27%;
+  bottom: -40px;
+  width: auto;
+  transform: rotate(90deg);
+  filter: hue-rotate(185deg) ;
+}
+
+.orange {
+  top: -40px;
+  filter: hue-rotate(0) ;
+}
+
 </style>
